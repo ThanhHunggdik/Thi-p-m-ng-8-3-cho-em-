c8fc342c85a4cd11bbca1ce75df320ae54ebb527
ThanhHunggdik <162668945+ThanhHunggdik@users.noreply.github.com>
2024-03-08 14:49:16 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Thị Hồng ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Gửi em. Cô gái tháng 1 của anh';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu em';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
