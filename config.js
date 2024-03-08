2a1e3a06812a09d2c26185e9b9b7ee8dae1f84c5
ThanhHunggdik <162668945+ThanhHunggdik@users.noreply.github.com>
2024-03-08 15:09:02 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Thị Hồng ';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
+const eventName = "Happy Women Day's 8-3";
 const titleCard = 'Gửi em. Cô gái tháng 1 của anh';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu em';
+const contentCard = 'Chúc honey của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu em';
 
 const giftImage = 'hot-girl.png';
 const base64 = '';
